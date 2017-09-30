5625495bffaa39ca70fdd138a00fe428bd8d3236
sahinerbay <sahin@sahinerbay.com>
2017-09-30 23:22:24 +0300

    HtmlWebpackPlugin added to create index.html dynamically
    	CleanWebpackPlugin added to recreate the build folder after
    	every run
    
    	sourcemap added to both css and js

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path');
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
     entry: ["babel-polyfill", "./src/index.js"],
@@ -7,34 +9,46 @@ module.exports = {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'build')
     },
+    devtool: 'inline-source-map',
     module: {
         rules: [{
-                test: /\.js$/,
-                exclude: /(node_modules|bower_components)/, //don't scan this file
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['env']
-                    }
+            test: /\.js$/,
+            exclude: /(node_modules|bower_components)/, //don't scan this file
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['env']
                 }
-            },
-            {
-                test: /\.css$/,
-                use: ExtractTextPlugin.extract({
-                    fallback: "style-loader",
-                    use: [{
-                            loader: 'css-loader',
-                            options: {
-                                importLoaders: 1
-                            }
-                        },
-                        'postcss-loader'
-                    ]
-                })
             }
+        },
+        {
+            test: /\.css$/,
+            use: ExtractTextPlugin.extract({
+                fallback: "style-loader",
+                use: [
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            importLoaders: 1,
+                            sourceMap: true
+                        }
+                    },
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            sourceMap: true
+                        }
+                    }
+                ]
+            })
+        }
         ]
     },
     plugins: [
         new ExtractTextPlugin("styles.css"), //renamed new file
+        new HtmlWebpackPlugin({
+            title: 'Output Management'
+        }),
+        new CleanWebpackPlugin(['build']),
     ]
 };
